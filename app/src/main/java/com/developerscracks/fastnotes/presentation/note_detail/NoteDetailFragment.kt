b2527c9d8524aad7ff836bf378a858fdd0d72c3c
Alejandro <faradius.magic187@gmail.com>
2023-03-21 13:52:55 -0600

    Configuración de los fragmentos con el navigation component

---
+++ app/src/main/java/com/developerscracks/fastnotes/presentation/note_detail/NoteDetailFragment.kt
@@ -0,0 +1,34 @@
+package com.developerscracks.fastnotes.presentation.note_detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.developerscracks.fastnotes.R
+import com.developerscracks.fastnotes.databinding.FragmentNoteDetailBinding
+import com.developerscracks.fastnotes.databinding.FragmentNoteListBinding
+
+class NoteDetailFragment : Fragment() {
+
+    private var _binding: FragmentNoteDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNoteDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
